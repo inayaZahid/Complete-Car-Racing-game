dea0ce2bc471e36a0c32d9383b74616e951ce08b
inayaZahid <82757716+inayaZahid@users.noreply.github.com>
2021-07-21 15:39:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,11 +14,11 @@ var track, car1_img, car2_img, car3_img, car4_img;
 
 function preload(){
   track = loadImage("track.jpg");
-  car1_img = loadImage("images/car1.png");
-  car2_img = loadImage("images/car2.png");
-  car3_img = loadImage("images/car3.png");
-  car4_img = loadImage("images/car4.png");
-  ground = loadImage("images/ground.png");
+  car1_img = loadImage("car1.png");
+  car2_img = loadImage("car2.png");
+  car3_img = loadImage("car3.png");
+  car4_img = loadImage("car4.png");
+  ground = loadImage("ground.png");
 }
 
 function setup(){
